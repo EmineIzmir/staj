fb70386cbe48e017d293ea10fc754be72c6ca5c1
EmineIzmir <emineizmir1990@gmail.com>
2015-02-05 13:54:01 +0200

    Signed-off-by: EmineIzmir <emineizmir1990@gmail.com>

---
+++ Parser/src/sikayetVar/File_Writer.java
@@ -0,0 +1,21 @@
+package sikayetVar;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+
+public class File_Writer {
+	public void createJson(ArrayList<Complaint> compObj, String fileName)
+			throws IOException {
+		Gson gson = new Gson();
+		String json = gson.toJson(compObj);
+		BufferedWriter out = new BufferedWriter(new FileWriter(fileName));
+		out.write(json);
+		out.newLine();
+		out.close();
+		System.out.println("Json created!");
+	}
+}
